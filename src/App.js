8d7f29764f2396b456998795d80458b0316e0c09
OnionPowder01 <autonatione36@gmail.com>
2023-03-02 23:42:23 +0200

    SettingsContext

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import Settings from "./components/Settings";
 import SettingsContext from "./components/SettingsContext";
 import Timer from "./components/Timer";
 
 function App() {
-
   const [showSettings, setShowSettings] = useState(false);
   const [workMinutes, setWorkMinutes] = useState(25);
-  const [breakMinutes, setBreakMinutes] = useState(5)
+  const [breakMinutes, setBreakMinutes] = useState(5);
 
   const settingsToggle = () => {
-    setShowSettings(prevState => !prevState )
-  
-  }
-
+    setShowSettings((prevState) => !prevState);
+  };
 
   return (
     <>
-    <main>
-      <SettingsContext.Provider value={{
-         workMinutes,
-         breakMinutes,
-         setWorkMinutes,
-         setBreakMinutes,
-      }}> 
-      {showSettings ? <Settings handleClick={settingsToggle} /> : <Timer handleClick={settingsToggle}/>}
-      </SettingsContext.Provider> 
-    </main>
+      <main>
+        <SettingsContext.Provider
+          value={{
+            showSettings,
+            setShowSettings,
+            workMinutes,
+            breakMinutes,
+            setWorkMinutes,
+            setBreakMinutes,
+          }}
+        >
+          {showSettings ? (
+            <Settings handleClick={settingsToggle} />
+          ) : (
+            <Timer handleClick={settingsToggle} />
+          )}
+        </SettingsContext.Provider>
+      </main>
     </>
   );
 }
